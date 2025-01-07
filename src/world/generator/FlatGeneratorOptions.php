7b1b35ab1f0bcfb2df1acf0670d286e9a19a130f
Dylan K. Taylor <dktapps@pmmp.io>
2025-01-07 22:07:38 +0000

    generator: fixup issues reported by PHPStan 2.0

--- src/world/generator/FlatGeneratorOptions.php
+++ src/world/generator/FlatGeneratorOptions.php
@@ -75,8 +75,7 @@ final class FlatGeneratorOptions{
 		$y = 0;
 		$itemParser = LegacyStringToItemParser::getInstance();
 		foreach($split as $line){
-			preg_match('#^(?:(\d+)[x|*])?(.+)$#', $line, $matches);
-			if(count($matches) !== 3){
+			if(preg_match('#^(?:(\d+)[x|*])?(.+)$#', $line, $matches) !== 1){
 				throw new InvalidGeneratorOptionsException("Invalid preset layer \"$line\"");
 			}
 
